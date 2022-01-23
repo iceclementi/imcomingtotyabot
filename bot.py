4f9632ab0e1497bb91f13bc4fb9542775b2f3561
iceclementi <iceclementi@gmail.com>
2022-01-23 17:56:24 +0800

    Change app name

--- bot.py
+++ bot.py
@@ -1,15 +1,12 @@
 import os
 import logging
-#from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from telegram import ext as telext
-from telext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 TOKEN = os.environ["TOKEN"]
 PORT = int(os.environ.get("PORT", 5000))
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
-
 
 
 def start(update, context):
@@ -21,9 +18,11 @@ def help_command(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text("Help!")
 
+
 def echo(update, context):
     """Echo the user's message."""
     update.message.reply_text(update.message.text)
+
 
 def error(update, context):
     """Log errors from Updates."""
@@ -49,7 +48,7 @@ def main():
 
     # Start the bot
     updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
-    updater.bot.setWebhook("https://yourherokuappname.herokuapp.com/" + TOKEN)
+    updater.bot.setWebhook("https://tya-countmein.herokuapp.com/" + TOKEN)
     updater.idle()
 
 
