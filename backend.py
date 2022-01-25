db559e1a25d5a0543c166da7239b10331e584e89
iceclementi <iceclementi@gmail.com>
2022-01-25 18:08:45 +0800

    Add admin buttons after done

--- backend.py
+++ backend.py
@@ -162,7 +162,6 @@ class Poll(object):
     def render_text(self) -> str:
         header = [make_html_bold_first_line(self.title)]
         body = [option.render_text() for option in self.options]
-        # footer = [f"{EMOJI_PEOPLE} {self.generate_respondents_summary}"]
         footer = [f"{EMOJI_PEOPLE} {self.generate_respondents_summary()}"]
         return "\n\n".join(header + body + footer)
 
