8697438787ccb7265053f301f3ff3809a94c079e
iceclementi <iceclementi@gmail.com>
2022-02-04 19:14:24 +0800

    Fix error in method to fetch credentials

--- database.py
+++ database.py
@@ -13,7 +13,7 @@ json_creds = os.getenv("GOOGLE_SHEETS_CREDS_JSON")
 
 creds_dict = json.loads(json_creds)
 creds_dict["private_key"] = creds_dict["private_key"].replace("\\\\n", "\n")
-creds = ServiceAccountCredentials.from_json_keyfile_name(creds_dict, scopes)
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
 client = gspread.authorize(creds)
 
 # endregion
