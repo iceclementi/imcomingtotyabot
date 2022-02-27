cc6b2528175278b481383edf1b804b285c2bd469
iceclementi <iceclementi@gmail.com>
2022-02-28 00:03:59 +0800

    Fix wrong type name

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ import re
 import typing
 from datetime import datetime
 from hashlib import blake2b as blake
-from typing import List as Lst, Tuple, Set, Union, Dict
+from typing import List, Tuple, Set, Union, Dict
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup
 
 ENCODE_KEY = string.digits + string.ascii_letters
