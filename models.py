11d1b3debc9291ed2598108dfcd8fbe2fa9e02be
iceclementi <iceclementi@gmail.com>
2022-02-28 23:48:07 +0800

    Fix wrong variable name

--- models.py
+++ models.py
@@ -1621,7 +1621,7 @@ class List(object):
 
     def build_choice_buttons(self, opt_id: int, page_number=0) -> InlineKeyboardMarkup:
         choice_button_group = PaginationButtonGroup(
-            self.choices, (LIST_SUBJECT, f"{CHOICE}_{opt_id}", list_id),
+            self.choices, (LIST_SUBJECT, f"{CHOICE}_{opt_id}", self.list_id),
             items_per_page=5, is_horizontal_buttons=True, is_cyclic=True
         )
         buttons = choice_button_group.build_buttons(page_number)
