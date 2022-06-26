7a8606cac4556e79315ecd2951a358612d6640bd
iceclementi <iceclementi@gmail.com>
2022-06-26 11:28:18 +0800

    Fix Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python start.py
+web: python telebot/start.py
--- webapp/server/db.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE test_database;
-
-CREATE TABLE todo (
-    todo_id SERI AL PRIMARY KEY,
-    description VARCHAR(255)
-);
